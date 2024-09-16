a056234d4ed1eaed53e812cda494c5d2d0f49384
Rajanikantupadhyay <rajanikantupadhyay07@gmail.com>
2024-09-16 14:45:28 +0530

    changing the title

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -18,7 +18,7 @@ const Main = () => {
   return (
     <div className='main'>
       <div className="nav">
-        <p>Gemini</p>
+        <p>Jarvis</p>
         <img src={assets.user_icon} alt="" />
       </div>
       <div className="main-container">
